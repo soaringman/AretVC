8f7f1a51666646bc1d8aa52c48289535c7fe91c2
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-24 13:22:41 +0300

    Add AlertController .alert

--- AretVC/ViewController.swift
+++ AretVC/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: "Error", message: "Oh Main Goth!", preferredStyle: .alert)
+        let actionButton = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil text")
+        }
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(actionButton)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
