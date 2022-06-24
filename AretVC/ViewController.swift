292abb9c10564a50255c2efe905f1809d59129f6
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-24 14:30:08 +0300

    Modify to universal alert method

--- AretVC/ViewController.swift
+++ AretVC/ViewController.swift
@@ -8,19 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var hiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func ButtonPressed(_ sender: Any) {
-        let alertController = UIAlertController(title: "Error", message: "Oh Main Goth!", preferredStyle: .alert)
-        let actionButton = UIAlertAction(title: "Ok", style: .default) { (action) in
-            let text = alertController.textFields?.first?.text
-            print(text ?? "nil text")
+        
+        self.hiLabel.text = "Hi,"
+        self.universalAlert(title: "Внимание", message: "Введите ваше имя", style: .alert)
+        
         }
-        alertController.addTextField(configurationHandler: nil)
+    
+    //Make universal alert method
+    func universalAlert (title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let actionButton = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.hiLabel.text? += (text?.text! ?? "") + ("!")
+            
+    }
+        alertController.addTextField { textField in
+            textField.textColor = .red
+        }
         alertController.addAction(actionButton)
         self.present(alertController, animated: true, completion: nil)
     }
